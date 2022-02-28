76ecfe5a9e18349e7a5983604185fcae706005f7
anumgowda <anu.xworkz@gmail.com>
2022-02-28 15:05:39 +0530

    upload Variables

---
+++ Variables/applephone.java
@@ -0,0 +1,22 @@
+class AppleIPhone13Mini
+{
+public static void main(String []arg){
+int prize=99900;
+String modelName="IPHONE";
+String Brand="Apple";
+String formFactor="SmartPhone";
+String memoryStorageCapacity="512GB";
+String operatingSystem="IOS14";
+String color="RED";
+String screenSize="5.4Inches";
+String cellularTechnology="GSM";
+System.out.println("prize :"+prize);
+System.out.println("modelName: "+ modelName );
+System.out.println("Brand:" + Brand);
+System.out.println("formFactor:" + formFactor);
+System.out.println("memoryStorageCapacity:" + memoryStorageCapacity);
+System.out.println("operatingSystem:" +operatingSystem);
+System.out.println("color:" + color);
+System.out.println("screenSize:" + screenSize);
+System.out.println("cellularTechnology:" + cellularTechnology);
+}}
