37f78721ce103fefd2f8b856efeb7fdef1d6dd47
anumgowda <anu.xworkz@gmail.com>
2022-03-01 10:47:08 +0530

    variables program

---
+++ Variable/cameravariable.java
@@ -0,0 +1,19 @@
+class SonyAlphaCamera{
+static String modelName="ILCE-7RM3A";
+static String brand="Sony";
+static String formFactor="Mirrorless";
+static String color="Black";
+static String ShootingModes="Auto";
+static String lensType="Prime";
+static double price=225950.00;
+public static void main(String []arg)
+{
+System.out.println("modelName  :" + modelName);
+System.out.println("brand  :  " + brand);
+System.out.println("formFactor  :" + formFactor);
+System.out.println("color  :" + color);
+System.out.println("ShootingModes  :" + ShootingModes);
+System.out.println("lensType  :" + lensType);
+System.out.println("price  :" + price);
+}
+}
