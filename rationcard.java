a8d901559388e2f07ed712f85441bbb7c23e6e89
anumgowda <anu.xworkz@gmail.com>
2022-03-20 12:13:16 +0530

    upload rationcard.java

---
+++ rationcard.java
@@ -0,0 +1,30 @@
+package com.xworkz.rationcard;
+
+public class rationcard {
+	private int rationcardno;
+	private int noOfMembers;
+	
+	
+	public rationcard()
+	{
+		
+	}
+public void setRationcardNo(int rationcardNo) {
+	this.rationcardno = rationcardNo;
+}
+public void setNoOfMembers(int noOfMembers) {
+	this.noOfMembers = noOfMembers;
+}
+
+
+@Override
+public String toString() {
+	return "rationcard---[rationcardno="+this.rationcardno+","+"noOfMemebers="+this.noOfMembers+"]";""
+	
+}
+public void setnoOfMembers(int i) {
+	// TODO Auto-generated method stub
+	
+}
+}
+
