9f98c3776af7d60776bdfbd58fd8f7395797d7fd
anumgowda <anu.xworkz@gmail.com>
2022-03-21 20:06:39 +0530

    split

---
+++ split.java
@@ -0,0 +1,13 @@
+package interview;
+
+public class split {
+	public static void main(String[] args) {
+		String s="hello how are u";
+		String[] words=s.split(" ");
+		for(String w:words) {
+			System.out.println(w);
+		}
+		
+	}
+
+}
