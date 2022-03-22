38fbc1590cb56215d6d2e553a9fd47e462e6b03b
anumgowda <anu.xworkz@gmail.com>
2022-03-22 06:38:17 +0530

    scannerapp

---
+++ ScannerApp.java
@@ -0,0 +1,23 @@
+package interview;
+
+import java.util.Scanner;
+
+public class ScannerApp {
+	public static void main(String []args) {
+		Scanner s=new Scanner(System.in);
+				System.out.println("enter a brand");
+		String brand=s.next();
+		System.out.println("enter a size");
+		int size=s.nextInt();
+		System.out.println("enter a color");
+		String color=s.next();
+		System.out.println("enter a prize");
+		float price =s.nextFloat();
+		System.out.println("enter a type of dress");
+		String dress=s.next();
+		
+		
+		
+	}
+
+}
